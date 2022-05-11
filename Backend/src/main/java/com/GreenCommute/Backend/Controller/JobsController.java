6b9f1b39a9b110be3be31521e059491c7b157dd9
PDhanrajnath <porikadhanrajnath@gmail.com>
2022-05-11 17:45:00 +0530

    added search functionality for jobs by skills

--- Backend/src/main/java/com/GreenCommute/Backend/Controller/JobsController.java
+++ Backend/src/main/java/com/GreenCommute/Backend/Controller/JobsController.java
@@ -2,6 +2,7 @@ package com.GreenCommute.Backend.Controller;
 
 
 import com.GreenCommute.Backend.Entity.Jobs;
+import com.GreenCommute.Backend.Helper.Helper;
 import com.GreenCommute.Backend.Service.JobServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +20,21 @@ public class JobsController {
     @Autowired
     private JobServiceImpl jobService;
 
+    @Autowired
+    private Helper helper;
+
     @GetMapping
-    public List<Jobs> getAllJobs(@RequestParam(value="location",required = false) String loc) {
-        if(loc==null){
-            return jobService.getAllJobs();
-        }
-        else{
+    public List<Jobs> getAllJobs(@RequestParam(value="location",required = false) String loc,@RequestParam(value="skill",required = false) String[] skill) {
+        List<Jobs> jobsList = jobService.getAllJobs();
+        if(loc==null && skill ==null){
+            return jobsList;
+        } else if (loc==null) {
+         return helper.getJobsSearchBySkills(jobsList,skill);
+        } else if (skill==null) {
             return jobService.getJobsSearchByLocation(loc);
+        } else{
+            List<Jobs> jobsByLoc =jobService.getJobsSearchByLocation(loc);
+            return helper.getJobsSearchBySkills(jobsByLoc,skill);
         }
     }
 
