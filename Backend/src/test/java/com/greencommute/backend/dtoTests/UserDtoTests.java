3f655efa9bc091881586613c15eb53a60a35201b
PDhanrajnath <porikadhanrajnath@gmail.com>
2022-05-17 21:19:32 +0530

    test cases6

--- Backend/src/test/java/com/greencommute/backend/dtoTests/UserDtoTests.java
+++ Backend/src/test/java/com/greencommute/backend/dtoTests/UserDtoTests.java
@@ -9,10 +9,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 class UserDtoTests {
 
     UserDto userDto = new UserDto(1,"userDTO");
+    UserDto userDto1 = new UserDto();
 
     @Test
     void userDtoEntityTest(){
         Assertions.assertEquals(1,userDto.getUserId());
         Assertions.assertEquals("userDTO",userDto.getUserName());
+
+        userDto1.setUserName("user2");
+        userDto1.setUserId(2);
+
+        Assertions.assertEquals(2,userDto1.getUserId());
+        Assertions.assertEquals("user2",userDto1.getUserName());
+
+        Assertions.assertEquals("UserDto(userId=2, userName=user2)",userDto1.toString());
     }
 }
