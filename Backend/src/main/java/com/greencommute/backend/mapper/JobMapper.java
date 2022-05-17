ba3edffcb9004e6927ebe4302ef379e5e4bff2b9
PDhanrajnath <porikadhanrajnath@gmail.com>
2022-05-17 21:31:14 +0530

    test cases7

--- Backend/src/main/java/com/greencommute/backend/mapper/JobMapper.java
+++ Backend/src/main/java/com/greencommute/backend/mapper/JobMapper.java
@@ -4,14 +4,11 @@ import com.greencommute.backend.dto.JobsDto;
 import com.greencommute.backend.entity.Jobs;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface JobMapper {
-
-    JobMapper INSTANCE = Mappers.getMapper(JobMapper.class);
 
     @Mapping(source = "jobId", target = "jobId")
     JobsDto toJobsDto(Jobs optionalJobs);
--- Backend/src/main/java/com/greencommute/backend/mapper/UserMapper.java
+++ Backend/src/main/java/com/greencommute/backend/mapper/UserMapper.java
@@ -4,12 +4,9 @@ import com.greencommute.backend.dto.UserDto;
 import com.greencommute.backend.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     @Mapping(source = "userId", target = "userId")
     UserDto toUserDto(User optionalUser);
